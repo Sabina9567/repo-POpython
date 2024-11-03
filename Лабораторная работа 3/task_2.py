b76fa6b2b7f386c8e510d150d7bd2ab2e8ba27a7
Sabina9567 <g.regina96@icloud.com>
2024-11-03 12:45:51 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_2.py
@@ -0,0 +1,16 @@
+def find_common_participants(participants_first_group, participants_second_group, separator=","):
+    first_group_list = participants_first_group.split(separator)
+    second_group_list = participants_second_group.split(separator)
+    common_participants = list(set(first_group_list) & set(second_group_list))
+    common_participants.sort()
+    return common_participants
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+common_participants = find_common_participants(participants_first_group, participants_second_group)
+print(f"Общие участники: {common_participants}")
+
+participants_first_group = "Иванов,Петров,Сидоров"
+participants_second_group = "Петров,Сидоров,Смирнов"
+common_participants = find_common_participants(participants_first_group, participants_second_group, separator=",")
+print(f"Общие участники: {common_participants}")
+# TODO Проверьте работу функции с разделителем отличным от запятой
